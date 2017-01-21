26c02a877d60a02f2b681003c3e286b34f796b95
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-01-21 03:52:39 -0800

    first route tests, config.js implemented for future database, and ports

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+
+
+
+
+app.use(express.static('public'));
+
+var config = require('./configure.js')
+
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// var config = require('./configure.js') // For sql database (cloud)
+
+app.get('/', function(req, res){
+	res.status(200).send("hello world bitch");
+
+
+});
+
+
+app.listen(config.PORT, function(){
+	console.log("Listening on port 8080");
+});
