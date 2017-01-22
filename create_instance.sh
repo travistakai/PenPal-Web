eddb710a49e5cbf9bd0d415418d9660745b3670c
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-01-21 18:08:12 -0800

    removed app engine setup

--- create_instance.sh
+++
@@ -1,7 +0,0 @@
-gcloud compute instances create my-app-instance \
-    --image=debian-8 \
-    --machine-type=g1-small \
-    --scopes userinfo-email,cloud-platform \
-    --metadata-from-file startup-script=gce/startup-script.sh \
-    --zone us-central1-f \
-    --tags http-server
